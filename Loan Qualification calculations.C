5ca90f6348f68d41b211b777b8c3647314caebb7
KINGp590 <157828224+KINGp590@users.noreply.github.com>
2024-10-01 23:59:59 +0300

    Update

---
+++ Loan Qualification calculations.C
@@ -0,0 +1,17 @@
+// Loan Qualification calculation
+#include <stdio.h>
+
+int main() {
+int age;
+float income;
+    
+printf("Enter your age: ");
+scanf("%d", &age);
+printf("Enter your annual income: ");
+scanf("%f", &income);
+
+if(age>=21&&income>=21000){printf("Congratulations, you qualify for a loan.\n");}
+else {printf("Unfortunately, we are unable to offer you a loan at this time.\n"); }
+
+return 0;
+}
