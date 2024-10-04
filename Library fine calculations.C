7cda7b47451e71e6c009cf945e12e05a0e2519f6
Rush <157828224+KINGp590@users.noreply.github.com>
2024-10-04 13:55:17 +0300

    Library fine calculations.C

--- Library fine calculations.C
+++ Library fine calculations.C
@@ -7,19 +7,21 @@ float fineRate, fineAmount;
 
 printf("Enter Book ID: ");
 scanf("%d", &bookID);
+
 printf("Enter Due Date (as an integer): ");
 scanf("%d", &dueDate);
+
 printf("Enter Return Date (as an integer): ");
 scanf("%d", &returnDate);
-
+// formula for days overdue 
 daysOverdue = returnDate - dueDate;
-
+//the conditions
 if (daysOverdue <= 7) {fineRate = 20; } 
 else if (daysOverdue <= 14) {fineRate = 50;} 
 else {fineRate = 100; }
-
+// formula for the fine amount 
 fineAmount = daysOverdue * fineRate;
-
+// the final output 
 printf("Book ID: %d\n", bookID);
 printf("Due Date: %d\n", dueDate);
 printf("Return Date: %d\n", returnDate);
