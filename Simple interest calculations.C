ae42ec64bb8491c0a60aab4411aa2e5f360d0e4e
Rush <157828224+KINGp590@users.noreply.github.com>
2024-10-04 14:48:04 +0300

    Update Simple interest calculations.C

--- Simple interest calculations.C
+++ Simple interest calculations.C
@@ -1,19 +1,22 @@
 //Simple interest calculation 
 #include <stdio.h>
 
-int main() { 
-float principal, rate, time, interest;
+int main() {
+ 
+ float principal, rate, time, interest;
     
-printf("Enter principal amount: ");
-scanf("%f", &principal);
-printf("Enter rate of interest: ");
-scanf("%f", &rate); 
-printf("Enter time period in years: ");
-scanf("%f", &time);
+ printf("Enter principal amount: ");
+ scanf("%f", &principal);
 
-interest = (principal * rate * time) / 100;
+ printf("Enter rate of interest: ");
+ scanf("%f", &rate);
 
-printf("Simple Interest = %.2f\n", interest); 
+ printf("Enter time period in years: ");
+ scanf("%f", &time);
 
+ //formula for simple interest 
+ interest = (principal * rate * time) / 100;
+
+ printf("Simple Interest = %.2f\n", interest); 
 return 0; 
 }
