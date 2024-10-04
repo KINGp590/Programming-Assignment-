9cbdee1db66d3e5be6e1181fc7142f7deaee304a
Rush <157828224+KINGp590@users.noreply.github.com>
2024-10-04 13:50:39 +0300

    Compound interest calculations.C

--- Compound interest calculations.C
+++ Compound interest calculations.C
@@ -1,5 +1,6 @@
 // Compound interest calculation
 #include <stdio.h>
+// adding maths library 
 #include <math.h>
 
 int main() {
@@ -8,15 +9,20 @@ int n, t;
 
 printf("Enter the principal amount (P): ");
 scanf("%lf", &P);
+
 printf("Enter the annual interest rate (r) (as a percentage): ");
 scanf("%lf", &r);
+
 printf("Enter the number of times interest applied per time period (n): ");
 scanf("%d", &n);
+
 printf("Enter the number of time periods elapsed (t): ");
 scanf("%d", &t);
+
+// making r a percentage 
     
 r = r / 100;
-
+//the compound interest formula 
 A = P * pow(1 + r / n, n * t);
     
 printf("The final amount after compound interest is: %.2lf\n", A);
