0a87e0238e9829ae9455e07b2130732a0d3beecb
Rush <157828224+KINGp590@users.noreply.github.com>
2024-10-06 13:26:28 +0300

    While loop.C

--- While loop.C
+++ While loop.C
@@ -1,11 +1,18 @@
-// while loop
+// while loop for no 1-50, their squares and sums
 #include <stdio.h>
 
 int main() {
-    int i = 1;
+    int i = 1, sum = 0, sumOfSquares = 0;
+
     while (i <= 50) {
-        printf(" %d\n", i);
+        sum += i; 
+        sumOfSquares += i * i;  
+        printf(" %d\t%d\n", i, i * i); 
         i++;
     }
+ 
+    printf("\nSum of numbers from 1 to 50: %d\n", sum);
+    printf("Sum of squares of numbers from 1 to 50: %d\n", sumOfSquares);
+    
     return 0;
 }
