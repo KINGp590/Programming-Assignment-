243d2965fe8f57b79355a61f92136f4a83c4d69b
Rush <157828224+KINGp590@users.noreply.github.com>
2024-10-06 13:22:58 +0300

    For loop.C

--- For loop.C
+++ For loop.C
@@ -1,9 +1,18 @@
-// for loop
+// for loop for no 1-50 , their squares and sums
 #include <stdio.h>
 
 int main() {
+    int sum = 0, sumOfSquares = 0;
+    
     for (int i = 1; i <= 50; i++) {
-        printf(" %d\n", i); 
+        sum += i; 
+        
+        sumOfSquares += i * i;  
+        printf("%d\t%d\n", i, i * i);  
     }
+   
+    printf("\nSum of numbers from 1 to 50: %d\n", sum);
+    printf("Sum of squares of numbers from 1 to 50: %d\n", sumOfSquares);
+    
     return 0;
 }
