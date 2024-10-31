57dd39422062895d9d796f7db1224e86ebdc410f
Kariuki Peter Waweru <157828224+KINGp590@users.noreply.github.com>
2024-10-31 15:55:02 +0300

    Update Append to data.txt.C

--- Append to data.txt.C
+++ Append to data.txt.C
@@ -7,15 +7,16 @@ void writeToFile() {
     
     fptr = fopen("data.txt", "w");
     if (fptr == NULL) {
-        printf("Error!");
+        printf("Oops! Couldn't open the file.\n");
         exit(1);
     }
     
-    printf("Enter a sentence:\n");
+    printf("Please enter a sentence you'd like to save:\n");
     fgets(sentence, sizeof(sentence), stdin);
     
     fprintf(fptr, "%s", sentence);
     fclose(fptr);
+    printf("Great! Your sentence has been saved.\n");
 }
 
 void readFromFile() {
@@ -24,12 +25,12 @@ void readFromFile() {
     
     fptr = fopen("data.txt", "r");
     if (fptr == NULL) {
-        printf("Error!");
+        printf("Hmm... Looks like there's nothing to read yet!\n");
         exit(1);
     }
     
     fgets(sentence, sizeof(sentence), fptr);
-    printf("Content of the file:\n%s", sentence);
+    printf("Here's what's in the file:\n%s", sentence);
     fclose(fptr);
 }
 
@@ -39,34 +40,43 @@ void appendToFile() {
     
     fptr = fopen("data.txt", "a");
     if (fptr == NULL) {
-        printf("Error!");
+        printf("Couldn't open the file for adding more text!\n");
         exit(1);
     }
     
-    printf("Enter a sentence to append:\n");
+    printf("Add another sentence to the file:\n");
     fgets(sentence, sizeof(sentence), stdin);
     
     fprintf(fptr, "%s", sentence);
     fclose(fptr);
+    printf("Done! Your sentence has been added.\n");
 }
 
 int main() {
     int choice;
 
     while (1) {
-        printf("\n1. Write to file\n");
-        printf("2. Read from file\n");
-        printf("3. Append to file\n");
+        printf("\nWhat would you like to do?\n");
+        printf("1. Write a new sentence to the file\n");
+        printf("2. Read what's in the file\n");
+        printf("3. Add more to the file\n");
         printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        getchar();
+        getchar(); 
         
-        if (choice == 1) writeToFile();
-        else if (choice == 2) readFromFile();
-        else if (choice == 3) appendToFile();
-        else if (choice == 4) exit(0);
-        else printf("Invalid choice.\n");
+        if (choice == 1) {
+            writeToFile();
+        } else if (choice == 2) {
+            readFromFile();
+        } else if (choice == 3) {
+            appendToFile();
+        } else if (choice == 4) {
+            printf("Goodbye!\n");
+            exit(0);
+        } else {
+            printf("That's not a valid option. Try again.\n");
+        }
     }
 
     return 0;
