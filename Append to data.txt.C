631ea537c7c81600fd447ef24e3e1aeaefed8c47
Kariuki Peter Waweru <157828224+KINGp590@users.noreply.github.com>
2024-10-31 16:25:40 +0300

    Manage sentences.C

--- Append to data.txt.C
+++ Append to data.txt.C
@@ -4,80 +4,70 @@
 void writeToFile() {
     char sentence[101];
     FILE *fptr;
-    
-    fptr = fopen("data.txt", "w");
+
+    fptr = fopen("C:\\Users\\user\\OneDrive\\Desktop\\assignments\\data.txt", "w");
     if (fptr == NULL) {
-        printf("Oops! Couldn't open the file.\n");
+        printf("Error opening file for writing!\n");
         exit(1);
     }
-    
-    printf("Please enter a sentence you'd like to save:\n");
+
+    printf("Enter a sentence (up to 100 characters):\n");
     fgets(sentence, sizeof(sentence), stdin);
     
+    if (sentence[0] == '\n') {
+        printf("No input provided. Exiting.\n");
+        fclose(fptr);
+        exit(1);
+    }
+
     fprintf(fptr, "%s", sentence);
     fclose(fptr);
-    printf("Great! Your sentence has been saved.\n");
 }
 
 void readFromFile() {
-    char sentence[101];
+    char ch;
     FILE *fptr;
-    
-    fptr = fopen("data.txt", "r");
+
+    fptr = fopen("C:\\Users\\user\\OneDrive\\Desktop\\assignments\\data.txt", "r");
     if (fptr == NULL) {
-        printf("Hmm... Looks like there's nothing to read yet!\n");
+        printf("Error opening file for reading!\n");
         exit(1);
     }
-    
-    fgets(sentence, sizeof(sentence), fptr);
-    printf("Here's what's in the file:\n%s", sentence);
+
+    printf("File content:\n");
+    while ((ch = fgetc(fptr)) != EOF) {
+        putchar(ch);
+    }
     fclose(fptr);
 }
 
 void appendToFile() {
     char sentence[101];
     FILE *fptr;
-    
-    fptr = fopen("data.txt", "a");
+
+    fptr = fopen("C:\\Users\\user\\OneDrive\\Desktop\\assignments\\data.txt", "a");
     if (fptr == NULL) {
-        printf("Couldn't open the file for adding more text!\n");
+        printf("Error opening file for appending!\n");
         exit(1);
     }
-    
-    printf("Add another sentence to the file:\n");
+
+    printf("Enter another sentence (up to 100 characters):\n");
     fgets(sentence, sizeof(sentence), stdin);
     
+    if (sentence[0] == '\n') {
+        printf("No input provided. Exiting.\n");
+        fclose(fptr);
+        exit(1);
+    }
+
     fprintf(fptr, "%s", sentence);
     fclose(fptr);
-    printf("Done! Your sentence has been added.\n");
 }
 
 int main() {
-    int choice;
-
-    while (1) {
-        printf("\nWhat would you like to do?\n");
-        printf("1. Write a new sentence to the file\n");
-        printf("2. Read what's in the file\n");
-        printf("3. Add more to the file\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        getchar(); 
-        
-        if (choice == 1) {
-            writeToFile();
-        } else if (choice == 2) {
-            readFromFile();
-        } else if (choice == 3) {
-            appendToFile();
-        } else if (choice == 4) {
-            printf("Goodbye!\n");
-            exit(0);
-        } else {
-            printf("That's not a valid option. Try again.\n");
-        }
-    }
-
+    writeToFile();
+    readFromFile();
+    appendToFile();
+    readFromFile();
     return 0;
 }
